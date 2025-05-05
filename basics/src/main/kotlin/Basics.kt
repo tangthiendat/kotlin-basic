502bf360595615934e189a3e3c1fe2f4157620df
Tang Thien Dat <thiendattang@gmail.com>
2025-05-05 15:48:11 +0700

    feat: if else

--- basics/src/main/kotlin/Basics.kt
+++ basics/src/main/kotlin/Basics.kt
@@ -10,8 +10,24 @@ fun main(){
 //    testVar = 600;
 //    println(testVar); //
 
-    //String
-    var name = "dat"
-    name = "test"
-    println(name.uppercase())
+//    //String
+//    var name = "dat"
+//    name = "test"
+//    println(name.uppercase())
+
+    print("Enter name: ")
+
+    var name = readln();
+
+    print("Enter age: ")
+
+    var age = readln().toInt();
+
+    if(age in 18..40){
+      println("Your name is $name. You're an adult")
+    } else if(age > 40){
+        println("Your name is $name. You're an elderly")
+    } else {
+        println("Your name is $name. You're not an adult\"")
+    }
 }
