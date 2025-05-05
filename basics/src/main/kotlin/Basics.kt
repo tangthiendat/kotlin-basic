3bd25dc2069ef556af173e1a317d3ca63095bb51
Tang Thien Dat <thiendattang@gmail.com>
2025-05-05 15:15:27 +0700

    feat: val vs var

---
+++ basics/src/main/kotlin/Basics.kt
@@ -0,0 +1,12 @@
+fun main(){
+    /** val vs var */
+    // val: immutable
+    val number = 1;
+//    number = 2 => not allow, error
+
+    // var: mutable
+    var testVar = 500;
+    println(testVar)
+    testVar = 600;
+    println(testVar); //
+}
