201bf81115986cbd80280d3c85100a607cb32216
Tang Thien Dat <thiendattang@gmail.com>
2025-05-05 15:30:18 +0700

    feat: string

--- basics/src/main/kotlin/Basics.kt
+++ basics/src/main/kotlin/Basics.kt
@@ -1,12 +1,17 @@
 fun main(){
-    /** val vs var */
-    // val: immutable
-    val number = 1;
-//    number = 2 => not allow, error
+//    /** val vs var */
+//    // val: immutable
+//    val number = 1;
+////    number = 2 => not allow, error
+//
+//    // var: mutable
+//    var testVar = 500;
+//    println(testVar)
+//    testVar = 600;
+//    println(testVar); //
 
-    // var: mutable
-    var testVar = 500;
-    println(testVar)
-    testVar = 600;
-    println(testVar); //
+    //String
+    var name = "dat"
+    name = "test"
+    println(name.uppercase())
 }
