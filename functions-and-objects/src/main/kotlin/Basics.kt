7bceb509bf52c0c56303f4311f80a655b795530d
Tang Thien Dat <thiendattang@gmail.com>
2025-05-05 16:46:57 +0700

    feat: function

---
+++ functions-and-objects/src/main/kotlin/Basics.kt
@@ -0,0 +1,11 @@
+fun main(){
+    makeCoffee();
+}
+
+fun makeCoffee(){
+    println("Buy beans")
+    println("Grind beans")
+    println("Heat water to 90°C")
+    println("Add water")
+    println("Pour water to ground beans")
+}
