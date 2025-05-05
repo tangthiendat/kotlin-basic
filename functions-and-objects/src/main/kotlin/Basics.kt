6fed4848444a16d2e9f41defabc7dfb98db89b88
Tang Thien Dat <thiendattang@gmail.com>
2025-05-05 17:10:30 +0700

    feat: function with parameter

--- functions-and-objects/src/main/kotlin/Basics.kt
+++ functions-and-objects/src/main/kotlin/Basics.kt
@@ -1,11 +1,18 @@
 fun main(){
-    makeCoffee();
+    println("Who is this coffee for?")
+    var name = readln();
+
+    println("How many pieces of sugar do you want?")
+
+    var sugarCount = readln().toInt();
+
+    makeCoffee(sugarCount, name)
 }
 
-fun makeCoffee(){
-    println("Buy beans")
-    println("Grind beans")
-    println("Heat water to 90°C")
-    println("Add water")
-    println("Pour water to ground beans")
+fun makeCoffee(sugarCount: Int, name: String){
+    if(sugarCount == 0){
+        println("Coffee with no sugar for $name")
+    }else {
+        println("Coffee with $sugarCount ${if (sugarCount > 1) "spoons" else "spoon"} for $name")
+    }
 }
