60b57ab62f1f59aa265b3f7df34b229492c303a7
Tang Thien Dat <thiendattang@gmail.com>
2025-05-05 17:48:29 +0700

    feat: data class

--- functions-and-objects/src/main/kotlin/Basics.kt
+++ functions-and-objects/src/main/kotlin/Basics.kt
@@ -1,20 +1,35 @@
+data class CoffeeDetails(
+    val sugarCount: Int,
+    val name: String,
+    val size: String,
+    val creamAmount: Int
+)
+
 fun main(){
+
+
+//    makeCoffee(sugarCount, name)
+    val coffeeForDat = CoffeeDetails(0, "Dat", "XL", 0)
+    makeCoffee(coffeeForDat)
+//    //Object
+//    var jack = Dog("Jack", "Dwarf poodle", 1);
+//    println("${jack.name} is a ${jack.breed} and is ${jack.age} years old")
+}
+
+//fun askCoffeeDetails(): CoffeeDetails{
 //    println("Who is this coffee for?")
 //    var name = readln();
 //
 //    println("How many pieces of sugar do you want?")
 //
 //    var sugarCount = readln().toInt();
-//
-//    makeCoffee(sugarCount, name)
-    var jack = Dog("Jack", "Dwarf poodle", 1);
-    println("${jack.name} is a ${jack.breed} and is ${jack.age} years old")
-}
+//}
 
-fun makeCoffee(sugarCount: Int, name: String){
-    if(sugarCount == 0){
-        println("Coffee with no sugar for $name")
+fun makeCoffee(coffeeDetails: CoffeeDetails){
+    if(coffeeDetails.sugarCount == 0){
+        println("Coffee with no sugar for ${coffeeDetails.name} and cream: ${coffeeDetails.creamAmount}")
     }else {
-        println("Coffee with $sugarCount ${if (sugarCount > 1) "spoons" else "spoon"} for $name")
+        println("Coffee with ${coffeeDetails.sugarCount} ${if (coffeeDetails.sugarCount > 1) "spoons" else "spoon"} for $${coffeeDetails.name} " +
+                "and cream: ${coffeeDetails.creamAmount}")
     }
 }
