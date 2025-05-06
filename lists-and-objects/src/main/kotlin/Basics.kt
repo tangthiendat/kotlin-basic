7311611fade16150148e9d21a94abf79d4ae5056
Tang Thien Dat <thiendattang@gmail.com>
2025-05-06 09:02:54 +0700

    feat: basic list

---
+++ lists-and-objects/src/main/kotlin/Basics.kt
@@ -0,0 +1,7 @@
+fun main() {
+    // Immutable list - you cannot add items after initialization
+//    var shoppingList = listOf("Processor", "RAM", "Graphics Card", "SSD")
+
+    // Mutable list - you can add items and modify
+    var shoppingList = mutableListOf("Processor", "RAM", "Graphics Card", "SSD")
+}
